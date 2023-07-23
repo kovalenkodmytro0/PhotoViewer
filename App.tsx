b678fb14148efd85c5a8edf8272fc909bfbbce9e
kovalenkodmytro0 <dmytrokovalenko708@gmail.com>
2023-07-23 22:03:34 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+   <View>
+    <Text></Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
